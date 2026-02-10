790fa7d1fe0747071f8f2767f2ca166ee552d5f2
Justin <justincox2710@gmail.com>
2026-02-10 10:23:26 -0800

    started some head to head capability

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from reminder import *
 def main():
     data = get_matches()
     nm = next_match(data)
+    h2h = head_to_head(nm)
     with sync_playwright() as playwright:
         take_screenshot(playwright)
-    send_message(nm)
+    send_message(nm, h2h)
 
 
 main()
--- reminder.py
+++ reminder.py
@@ -6,13 +6,13 @@ import json
 from datetime import *
 import time
 from playwright.sync_api import sync_playwright, Playwright
-
-
+#make this global so i can use it in other funcitons
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 
 def get_matches():
-    load_dotenv()
-    api_key = os.getenv("API_KEY")
+    
 
     #arsneal team id: 3068
     url = "https://api.soccerdataapi.com/matches"
@@ -28,14 +28,15 @@ def get_matches():
 def next_match(data: dict):
     now = datetime.now()
     fromatted_date_int = int(now.strftime("%Y%m%d"))
-    arsenal_matches = []
     filtered_matches = []
+    arsenal_matches = []
     for m in data:
         for i in m["stage"][0]["matches"]:
             if i["teams"]["home"]["name"] == "Arsenal" or i["teams"]["away"]["name"] == "Arsenal":
                 arsenal_matches.append(i)
     
     for i in arsenal_matches:
+        i["date"] = i["date"].replace("/", "-")
         temp_date = datetime.strptime(i["date"], "%d-%m-%Y")
         temp_format = int(datetime.strftime(temp_date, "%Y%m%d"))
 
@@ -46,8 +47,22 @@ def next_match(data: dict):
     print(f"{next_match["date"]} {next_match["teams"]["home"]["name"]} vs {next_match["teams"]["away"]["name"]}")
     return next_match
 
-def send_message(next_match):
-    message = f"Arsenal play on {next_match["date"]} Match: {next_match["time"]} {next_match["teams"]["home"]["name"]} vs {next_match["teams"]["away"]["name"]}"
+def head_to_head(next_match: dict):
+    home = next_match["teams"]["home"]["id"]
+    away = next_match["teams"]["away"]["id"]
+    url = "https://api.soccerdataapi.com/head-to-head"
+    querystring = {'team_1_id': home, 'team_2_id': away,'auth_token': api_key}
+    headers = {
+        'Accept-Encoding': 'gzip',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    return data
+
+def send_message(next_match:dict, h2h:dict):
+    message = f"Arsenal play on {next_match["date"]} Match: {next_match["time"]} {next_match["teams"]["home"]["name"]} vs {next_match["teams"]["away"]["name"]} {h2h}"
     with open("screenshots/lineup.png", 'rb') as f:
         requests.post(
             "https://ntfy.sh/ArsenalReminder", 
@@ -70,6 +85,3 @@ def take_screenshot(playwright: Playwright):
     time.sleep(2)
     page.screenshot(path="screenshots/lineup.png")
     browser.close()
-
-    
-
