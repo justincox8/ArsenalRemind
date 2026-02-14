66ba0502011ea292b6adf124a34e7e3923bf4ba6
Justin <justincox2710@gmail.com>
2026-02-13 18:07:57 -0800

    fixed some things i missed

--- reminder.py
+++ reminder.py
@@ -6,17 +6,14 @@ def main():
     data = get_matches()
     nm = next_match(data)
     is_today = check_date(nm)
-    if(is_today == False):
+    if(is_today == True):
         wait_time(nm)
         send_message()
     else:
         print("No arsenal game today")
 
-"""schedule.every().day.at("02:45", "America/Los_Angeles").do(main)
+schedule.every().day.at("02:45", "America/Los_Angeles").do(main)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-"""
-
-main()
